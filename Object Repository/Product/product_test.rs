d8cd8f43dd89c0b3cd8950dfca5720950627aed6
Emmanuel Daud <haryantodaud655@gmail.com>
2026-04-09 16:30:54 +0700

    add new test case

---
+++ Object Repository/Product/product_test.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>product_test</name>
+   <tag></tag>
+   <elementGuidId>04694a7c-ab61-439c-b7a3-0e0bd0278a0b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@data-test = 'inventory-item-sauce-labs-bike-light-img']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-test</name>
+      <type>Main</type>
+      <value>inventory-item-sauce-labs-bike-light-img</value>
+      <webElementGuid>704c8b31-da17-4dc4-9cb8-41dcc052307d</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
